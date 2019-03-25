75e31f8271911e9a399fc148656ac77a7d3fd5de
rk62pt <rk62pt@gmail.com>
2019-03-25 17:32:52 +0800

    2 files modified

--- src/app/cus-message/cus-message.component.ts
+++ src/app/cus-message/cus-message.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MAT_DIALOG_DATA, MatIconRegistry } from '@angular/material';
 import { CusMessageConfig } from './cus-message-config';
 
 @Component({
